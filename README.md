287f7316656b55e4646a8921f68f7a50d667245a
Adrian May <adrian.alexander.may@gmail.com>
2020-04-27 09:04:47 +0100

    Relaying video but 2 steps forward and one step back

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # mojo
 Motion detector with velocity-of-centre-of-gravity algorithm
 
+Many thanks to camodet_python where half of this code is copied from.
 
 Usage:
 
 docker build -t mojo .
 
-docker run -it --network host -v /home/ad/junk:/out mojo
+docker run -it --network host -e CAM=<cam> -v /home/ad/junk:/out mojo
+
+where <cam> is the url to the stream from a cam. Could probably be a file too. Not sure how to get at a local cam in docker.
+
+
+
 
--- mojo.py
+++ mojo.py
@@ -4,13 +4,41 @@ import cv2
 import time
 import sys
 import subprocess
+import os
 
 def main():
 
-    name = "/out/mojo.avi"
-    width = height = meanfps = 100
+    cam = os.environ['CAM']
+    cap = cv2.VideoCapture(cam, cv2.CAP_ANY)
+    if not cap.isOpened():
+        raise IOError("Cannot open the selected video/camera")
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    print ("Video Height {0} Width {1}".format(height, width))
+
+    tframe0 = time.time()
+    tframe1 = tframe0
+    fps = 0
+    meanfps = 1.0 # guess this for your cam
+
+    outname = "/out/mojo.avi"
     codec = cv2.VideoWriter_fourcc(*'MPEG')
-    output = cv2.VideoWriter(name, cv2.CAP_FFMPEG, codec, meanfps, (width,height))
+    output = cv2.VideoWriter(outname, cv2.CAP_FFMPEG, codec, meanfps, (width,height))
+
+    wantframes=10
+
+    while(wantframes>0):
+        hasframes, frame = cap.read()
+        if(not hasframes):
+            continue
+        elif(np.shape(frame) == ()):
+            continue
+        elif(np.sum(frame) == 0):
+            continue
+        else: 
+            wantframes=wantframes-1
+            output.write(frame)
+
     output.release()
 
 if __name__ == '__main__':
